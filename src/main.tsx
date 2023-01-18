5a0e829c865a5c9462cf1452ec073e32c9802297
Evgeny Berg <berg.evg.1@gmail.com>
2023-01-19 00:24:52 +0400

    deleted extra files and code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./style.css";
+
+import "./style.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
